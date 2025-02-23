929a3033fbb39b298bbda8762f5ce2f645807cc7
Zachary Butler <zacharybutler@Zacharys-MacBook-Pro.local>
2025-02-22 21:21:05 -0500

    basic header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Header from './components/header.jsx'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Header />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
