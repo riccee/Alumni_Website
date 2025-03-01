2ca9274b26b34b6f8547fac0203ad9af4d5842f7
ubuntu <ubuntu@amhsalumni.com>
2025-03-01 18:08:27 -0500

    updated header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,34 +1,101 @@
-import React from "react";
-import { Box } from "@mui/material";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import Avatar from "@mui/material/Avatar";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
 
-const Header = ({ isAuthenticated }) => {
-    return (
+const settings = ["Logout"];
+
+const Header = ({ isAuthenticated, onLogout }) => {
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenUserMenu = (event) => {
+    event.preventDefault();
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar position="sticky" sx={{ backgroundColor: "#14542c" }}>
+      <Toolbar
+        disableGutters
+        sx={{
+          justifyContent: "space-between",
+          height: 80,
+        }}
+      >
         <Box
-            component="header"
-            sx={{
-                position: "sticky",
-                top: 0,
-                zIndex: 9999,
-                width: "100%",
-                bgcolor: "#14542c",
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                p: 2,
-                boxShadow: "0px 2px 4px rgba(0,0,0,0.1)",
-            }}
+          sx={{
+            top: 0,
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            p: 2,
+          }}
         >
-            <img
-                src="https://c.animaapp.com/lWfAepnV/img/image-4@2x.png"
-                alt="Logo"
-                style={{ width: 58, height: 58 }}
-            />
-            {isAuthenticated && (
-                <AccountCircleIcon sx={{ color: "white", fontSize: 58 }} />
-            )}
+          <img
+            src="https://c.animaapp.com/lWfAepnV/img/image-4@2x.png"
+            alt="Logo"
+            style={{ width: 58, height: 58 }}
+          />
         </Box>
-    );
-};
 
+        {isAuthenticated && (
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              p: 2,
+            }}
+          >
+            <Tooltip>
+              <IconButton onClick={handleOpenUserMenu}>
+                <Avatar src="/broken-image.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "60px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem
+                  key={setting}
+                  onClick={() => {
+                    onLogout();
+                    handleCloseUserMenu();
+                  }}
+                >
+                  <Typography sx={{ textAlign: "center" }}>
+                    {setting}
+                  </Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};
 export default Header;
